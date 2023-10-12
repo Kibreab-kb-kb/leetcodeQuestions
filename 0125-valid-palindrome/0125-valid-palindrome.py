f6b110ef21a1817ba1d37c09682f543ef386c526
KibreabTeshome <114220350+Kibreab-kb-kb@users.noreply.github.com>
2023-10-12 17:40:15 +0300

    Time: 45 ms (81.32%), Space: 17.6 MB (34.26%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a=[]
+        s=s.lower()
+        for i in s:
+            if(i.isalnum()):
+                a.append(i)
+        #print(a)
+        if(a==a[::-1]):
+            return True
+        else:
+            return False
+        
+        
+        
